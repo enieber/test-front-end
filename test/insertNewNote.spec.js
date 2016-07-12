06213efef72926d884cf23879623aa5e40a4ca7d
Enieber Cunha <eniebercunha@gmail.com>
2016-07-12 18:22:48 -0300

    add tests with success

---
+++ test/insertNewNote.spec.js
@@ -0,0 +1,28 @@
+describe('Insert Note', function() {
+
+  before(function() {    
+    casper.start('http://localhost:8080/')
+  })
+
+  it('deve ter o title Ceep', function() {
+    casper.then(function() {
+      'Ceep'.should.matchTitle
+    }) 
+  })  
+
+  it('deve ter uma textarea', function() {  
+    casper.then(function() {
+      '.insereNovaNota-campo'.should.be.inDOM.and.be.visible
+    })
+  })  
+
+  it('deve inserir um dado no textarea', function() {
+    casper.sendKeys('textarea', 'aaa') 
+    expect('textarea').to.have.fieldValue('aaa')
+  })
+
+  it('deve adicionar uma nova nota', function() {
+    casper.click('.insereNovaNota-envia')
+    expect('.nota-conteudo').to.have.text('aaa')
+  })
+})
